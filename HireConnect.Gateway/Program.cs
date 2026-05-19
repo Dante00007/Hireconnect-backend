32c3cee991f9e2b0bbd17bafe4445f44de7b61a7
Krishna Sharma <ks1658759@gmail.com>
2026-05-19 22:31:44 +0530

    Gateway health update

--- HireConnect.Gateway/Program.cs
+++ HireConnect.Gateway/Program.cs
@@ -7,20 +7,25 @@ builder.Services.AddHealthChecks();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("HireConnectPolicy", policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:4200") // Your Angular URL
+        policy.WithOrigins(
+            "http://localhost:4200",
+             "https://hireconnect-frontend.onrender.com"
+            ) 
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials(); // MECHANICAL NECESSITY for HttpOnly cookies 
     });
 });
 
+
 var app = builder.Build();
 
-app.UseCors("HireConnectPolicy");
+app.UseCors("AllowFrontend");
 
 app.UseWebSockets();
 app.MapReverseProxy();
+app.UseHealthChecks("/health");
 
 app.Run();
